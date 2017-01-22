53eb3f7b01c09e8c073e04258cd9afeae3e514b8
Xasin <logger89@web.de>
2017-01-22 15:35:37 +0100

    Forgot to add PositionCall.java!

---
+++ IWW/src/iww/PositionCall.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iww;
+
+/**
+ *
+ * @author xasin
+ */
+public interface PositionCall {
+    public void click(int x, int y);
+}
